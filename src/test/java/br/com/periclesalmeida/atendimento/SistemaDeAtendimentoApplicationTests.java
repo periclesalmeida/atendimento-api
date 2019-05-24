80eae437fc092e659b2ff79a1636b31ef3b112c4
Péricles Almeida Jr <jose.pericles.almeida@gmail.com>
2019-05-24 11:58:11 -0300

    Função de rodar todos os testes é da SistemaDeAtendimentoApplicationTests

--- src/test/java/br/com/periclesalmeida/atendimento/SistemaDeAtendimentoApplicationTests.java
+++ src/test/java/br/com/periclesalmeida/atendimento/SistemaDeAtendimentoApplicationTests.java
@@ -1,16 +1,16 @@
 package br.com.periclesalmeida.atendimento;
 
-import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+		UnitTests.class,
+		IntegrationTests.class
+})
 public class SistemaDeAtendimentoApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
 
 }
