476184664730f02af1dc04c5b632b1b89049dcd1
Péricles Almeida Jr <jose.pericles.almeida@gmail.com>
2019-09-09 21:36:02 +0100

    Adição de classe de configuração do Mongock e Classe de ChangeLog.

---
+++ src/main/java/br/com/periclesalmeida/atendimento/config/MongockChangeLog.java
@@ -0,0 +1,65 @@
+package br.com.periclesalmeida.atendimento.config;
+
+import br.com.periclesalmeida.atendimento.domain.Permissao;
+import br.com.periclesalmeida.atendimento.domain.Usuario;
+import com.github.cloudyrock.mongock.ChangeLog;
+import com.github.cloudyrock.mongock.ChangeSet;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
+@ChangeLog
+@Component
+public class MongockChangeLog {
+
+    @ChangeSet(id= "1.0", order = "001", author = "periclesalmeida")
+    public void changeSet1(MongoTemplate mongoTemplate) {
+        List<Permissao> permissoes =
+                Arrays.asList(
+                        new Permissao("ROLE_TIPO_LOCALIZACAO_CONSULTAR" ,"CONSULTAR TIPO DOCUMENTO"),
+                        new Permissao("ROLE_TIPO_LOCALIZACAO_CONSULTAR","CONSULTAR TIPO DOCUMENTO"),
+                        new Permissao("ROLE_TIPO_LOCALIZACAO_INCLUIR","INCLUIR TIPO DOCUMENTO"),
+                        new Permissao("ROLE_TIPO_LOCALIZACAO_ALTERAR","ALTERAR TIPO DOCUMENTO"),
+                        new Permissao("ROLE_LOCALIZACAO_CONSULTAR","CONSULTAR LOCALIZAÇÃO"),
+                        new Permissao("ROLE_LOCALIZACAO_INCLUIR","INCLUIR LOCALIZAÇÃO"),
+                        new Permissao("ROLE_LOCALIZACAO_ALTERAR","ALTERAR LOCALIZAÇÃO"),
+                        new Permissao("ROLE_SERVICO_CONSULTAR","CONSULTAR SERVIÇO"),
+                        new Permissao("ROLE_SERVICO_INCLUIR","INCLUIR SERVIÇO"),
+                        new Permissao("ROLE_SERVICO_ALTERAR","ALTERAR SERVIÇO"),
+                        new Permissao("ROLE_PERMISSAO_CONSULTAR","CONSULTAR PERMISSÃO"),
+                        new Permissao("ROLE_USUARIO_CONSULTAR","CONSULTAR USUÁRIO"),
+                        new Permissao("ROLE_USUARIO_INCLUIR","INCLUIR USUÁRIO"),
+                        new Permissao("ROLE_USUARIO_ALTERAR","ALTERAR USUÁRIO")
+                );
+
+        mongoTemplate.insertAll(permissoes);
+    }
+
+    @Profile("dev")
+    @ChangeSet(id= "1.1", order = "002", author = "periclesalmeida")
+    public void changeSetDev(MongoTemplate mongoTemplate) {
+        List<Permissao> permissoes =
+                Arrays.asList(
+                        new Permissao("ROLE_TIPO_LOCALIZACAO_CONSULTAR" ,"CONSULTAR TIPO DOCUMENTO"),
+                        new Permissao("ROLE_TIPO_LOCALIZACAO_CONSULTAR","CONSULTAR TIPO DOCUMENTO"),
+                        new Permissao("ROLE_TIPO_LOCALIZACAO_INCLUIR","INCLUIR TIPO DOCUMENTO"),
+                        new Permissao("ROLE_TIPO_LOCALIZACAO_ALTERAR","ALTERAR TIPO DOCUMENTO"),
+                        new Permissao("ROLE_LOCALIZACAO_CONSULTAR","CONSULTAR LOCALIZAÇÃO"),
+                        new Permissao("ROLE_LOCALIZACAO_INCLUIR","INCLUIR LOCALIZAÇÃO"),
+                        new Permissao("ROLE_LOCALIZACAO_ALTERAR","ALTERAR LOCALIZAÇÃO"),
+                        new Permissao("ROLE_SERVICO_CONSULTAR","CONSULTAR SERVIÇO"),
+                        new Permissao("ROLE_SERVICO_INCLUIR","INCLUIR SERVIÇO"),
+                        new Permissao("ROLE_SERVICO_ALTERAR","ALTERAR SERVIÇO"),
+                        new Permissao("ROLE_PERMISSAO_CONSULTAR","CONSULTAR PERMISSÃO"),
+                        new Permissao("ROLE_USUARIO_CONSULTAR","CONSULTAR USUÁRIO"),
+                        new Permissao("ROLE_USUARIO_INCLUIR","INCLUIR USUÁRIO"),
+                        new Permissao("ROLE_USUARIO_ALTERAR","ALTERAR USUÁRIO")
+                );
+
+        mongoTemplate.insert(new Usuario("admin", "C4CA4238A0B923820DCC509A6F75849B", true, new HashSet<>(permissoes)));
+    }
+}
---
+++ src/main/java/br/com/periclesalmeida/atendimento/config/MongockConfig.java
@@ -0,0 +1,20 @@
+package br.com.periclesalmeida.atendimento.config;
+
+import com.github.cloudyrock.mongock.SpringBootMongock;
+import com.github.cloudyrock.mongock.SpringBootMongockBuilder;
+import com.mongodb.MongoClient;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MongockConfig {
+
+    @Bean
+    public SpringBootMongock mongock(ApplicationContext springContext, MongoClient mongoClient) {
+        return new SpringBootMongockBuilder(mongoClient, "atendimento", "br.com.periclesalmeida.atendimento.config")
+                .setApplicationContext(springContext)
+                .setLockQuickConfig()
+                .build();
+    }
+}
