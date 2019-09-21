8b453635d588954a58a7cf635037ac87cd8765f7
Péricles Almeida Jr <jose.pericles.almeida@gmail.com>
2019-09-21 21:44:36 +0100

    Alteração do MongoConfig adição da propriedade database.

--- src/main/java/br/com/periclesalmeida/atendimento/config/MongoConfig.java
+++ src/main/java/br/com/periclesalmeida/atendimento/config/MongoConfig.java
@@ -3,16 +3,20 @@ package br.com.periclesalmeida.atendimento.config;
 import com.github.cloudyrock.mongock.SpringBootMongock;
 import com.github.cloudyrock.mongock.SpringBootMongockBuilder;
 import com.mongodb.MongoClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class MongockConfig {
+public class MongoConfig {
+
+    @Value("${spring.data.mongodb.database}")
+    private String database;
 
     @Bean
     public SpringBootMongock mongock(ApplicationContext springContext, MongoClient mongoClient) {
-        return new SpringBootMongockBuilder(mongoClient, "atendimento", "br.com.periclesalmeida.atendimento.config.changelog")
+        return new SpringBootMongockBuilder(mongoClient, database, "br.com.periclesalmeida.atendimento.config.changelog")
                 .setApplicationContext(springContext)
                 .setLockQuickConfig()
                 .build();
